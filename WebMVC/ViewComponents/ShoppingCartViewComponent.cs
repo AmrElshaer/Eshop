bc424b8af5afdffa8665c8cec2f368fd6c58f071
Amr Elshaer <amrelsher07@gmail.com>
2022-04-20 13:32:37 +0200

    adding shopping cart

---
+++ WebMVC/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.IServices;
+
+namespace WebMVC.ViewComponents
+{
+    public class ShoppingCartViewComponent:ViewComponent
+    {
+        private readonly IBasketService _basketService;
+
+        public ShoppingCartViewComponent(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           return View(await _basketService.GetBasket("amr")) ;
+        }
+    }
+}
