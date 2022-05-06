3212901d1f335544b576495f31030300b9e2a046
Amr Elshaer <amrelsher07@gmail.com>
2022-05-06 21:37:52 +0200

    add logout

---
+++ WebMVC/Pages/Logout.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebMVC.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            return RedirectToPage("Index");
+        }
+    }
+}
